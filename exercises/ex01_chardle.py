b8f75e97f20b0b1212422808367825802a18676f
Shivani Kotikalapudi <shivkoti@unc.edu>
2023-01-30 21:09:43 -0500

    Progress on Exercise 1

---
+++ exercises/ex01_chardle.py
@@ -0,0 +1,34 @@
+"""EX01 - Chardle - A cute step toward Wordle."""
+
+__author__ = "730556602"
+
+word: str =  input ("Enter a 5-character word: " )
+number: int = 0
+if len(word) != 5:
+    print("Error: Word must contain 5 characters")
+    exit 
+else:
+    letter: str = input ("Enter a single character: ")
+    if len(letter) != 1:
+        print("Error: Character must be a single character.")
+        exit
+    else: print ("Searching for " + letter + " in " + word)
+    if letter==word[0]:
+        print(letter + " found at index 0")
+        number = number + 1
+    if letter==word[1]:
+        print(letter + " found at index 1") 
+        number = number + 1
+    if letter==word[2]:
+        print(letter + " found at index 2")
+        number = number + 1
+    if letter==word[3]:
+        print(letter + " found at index 3")
+        number = number + 1 
+    if letter==word[4]:
+        print(letter + " found at index 4")
+        number = number + 1
+    if number > 0:
+        print(str(number) + " instances of " + letter + " found in " + word)
+    else: print("No instances of " + letter + " found in " + word)
+
