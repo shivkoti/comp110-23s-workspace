352f1b5d3eb148c6828d8c2e38925bfebb73fc89
Shivani Kotikalapudi <shivkoti@unc.edu>
2023-03-01 10:14:57 -0500

    progress

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -1,7 +1,8 @@
-"""`list` Utility Functions"""
+"""`list` Utility Functions."""
 ___author___: 730556602
 
-def all(my_list: list[int],number: int) -> bool:
+def all(my_list: list[int],number: int) -> bool:  
+    """Indicates if all numbers in list match indicated number."""
     counter: int = 0
     if len(my_list) == 0:
         return False
@@ -12,8 +13,9 @@ def all(my_list: list[int],number: int) -> bool:
         
     return True
         
-def max(list_0: list[int]) -> int:
-    if len(input) == 0:
+def max(list_0: list[int]) -> int:  
+    """ Returns the largest number in the list."""
+    if len(list_0) == 0:
         raise ValueError("max() arg is an empty List")
     max_number = list_0[0]
     counter: int = 0
@@ -23,13 +25,11 @@ def max(list_0: list[int]) -> int:
         counter = counter + 1
     return max_number
 
-def is_equal(list_1: list[int], list_2: list[int]) -> bool:
+def is_equal(list_1: list[int], list_2: list[int]) -> bool:  
+    """True if lists are equal, False otherwise."""
     x: int = 0
     while x < len(list_1) and x < len(list_2):
         if list_1[x] != list_2[x]:
             return False
         x = x + 1
     return True
-
-if __name__ == "__is_equal__":
-    is_equal()
