fd7a556b8ea34c69f05413229afe44e0f9dff03a
Shivani Kotikalapudi <shivkoti@unc.edu>
2023-03-01 10:29:46 -0500

    progress

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -1,10 +1,9 @@
 """`list` Utility Functions."""
 ___author___: 730556602
 
-def all(my_list: list[int],number: int) -> bool:
-    
-      
+def all(my_list: list[int],number: int) -> bool:  
     """Indicates if all numbers in list match indicated number."""
+
     counter: int = 0
     if len(my_list) == 0:
         return False
@@ -16,9 +15,8 @@ def all(my_list: list[int],number: int) -> bool:
     return True
         
 def max(list_0: list[int]) -> int:
-
-
     """Returns the largest number in the list."""
+
     if len(list_0) == 0:
         raise ValueError("max() arg is an empty List")
     max_number = list_0[0]
@@ -30,9 +28,8 @@ def max(list_0: list[int]) -> int:
     return max_number
 
 def is_equal(list_1: list[int], list_2: list[int]) -> bool:
-
-
     """True if lists are equal, False otherwise."""
+    
     x: int = 0
     while x < len(list_1) and x < len(list_2):
         if list_1[x] != list_2[x]:
