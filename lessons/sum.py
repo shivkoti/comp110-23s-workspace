e97ad794d61e710b359baa4cae9582fd5baf1d9c
Shivani Kotikalapudi <shivkoti@unc.edu>
2023-03-02 14:57:13 -0500

    progress

--- lessons/sum.py
+++ lessons/sum.py
@@ -1,10 +1,9 @@
 """Example function for unit tests"""
 
 def sum(xs: list[float]) -> float:
-    """return sum of all elements in xs"""
-    sum_total: float = 0.0 
-    idx: int = 0
-    while idx < len(xs):
-        sum_total += xs[idx]
-        idx += 1
+    """return sum of all elements in xs""" 
+    sum_total: float = 0.0
+    for elem in xs:
+        sum_total += elem
     return sum_total
+    
