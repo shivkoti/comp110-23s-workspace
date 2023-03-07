333bfc8da76f7d7af8293eea13d86aaa7d3fb8c3
Shivani Kotikalapudi <shivkoti@unc.edu>
2023-03-07 14:17:11 -0500

    progress

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -4,8 +4,8 @@ __author__: "730556602"
 def only_evens(xs: list[int]) -> list[int]:
     """Returns even numbers in list"""
     for idx in xs:
-        if  xs[idx] %2 == 0:
-            xs.append(xs[idx])
+        if  idx %2 == 0:
+            xs.append(idx)
         else:
             return xs.append([])
     return xs  
@@ -18,11 +18,9 @@ def sub(a_list: list[int], a: int, b: int) -> list[int]:
     """Returns a List which is a subset of the given list"""
     if a < 0:
         a = 0
-    if b > len(a_list):
-        b = len(a_list)
-    if len(a_list) == 0: 
-        return a_list[()]
-    if a >= len(a_list):
-        return a_list[()]
-    for list in range(0, len(a_list)):
+    if b >= len(a_list):
+        b = len(a_list)  
+    if len(a_list) == 0 or a >= len(a_list): 
+        return a_list()
+    for list in range(a_list[0], len(a_list)):
         return [a_list[a], a_list[b]]
