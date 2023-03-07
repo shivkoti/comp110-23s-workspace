2ac1f8fc5b5061ca1d19a53013a3cebb3be3c937
Shivani Kotikalapudi <shivkoti@unc.edu>
2023-03-06 20:47:19 -0500

    progress

---
+++ exercises/ex05/utils.py
@@ -0,0 +1,27 @@
+
+__author__: "730556602"
+
+def only_evens(xs: list[int]) -> list[int]:
+    """Returns even numbers in list"""
+    for idx in xs:
+        if  xs[idx] %2 == 0:
+            xs.append(xs[idx])
+        else:
+            return xs.append([])
+    return xs  
+
+def concat(list1: list[int], list2: list[int]) -> list[int]:
+    """Concatenates two lists of ints and returns the resulting list."""
+    return list1 + list2
+
+def sub(a_list: list[int], a: int, b: int) -> list[int]:
+    """Returns a List which is a subset of the given list"""
+    if a < 0:
+        a = 0
+    if b > len(a_list):
+        b = len(a_list)
+    if len(a_list) == 0 or a >= len(list):
+        return a_list[] 
+    for list in range(0, len(a_list)):
+        return [a_list[a], a_list[b]]
+    
--- lessons/sum.py
+++ lessons/sum.py
@@ -3,7 +3,7 @@
 def sum(xs: list[float]) -> float:
     """return sum of all elements in xs""" 
     sum_total: float = 0.0
-    for elem in xs:
-        sum_total += elem
+    for idx in range(0, len(xs)):
+        sum_total += xs[idx]
     return sum_total
     
