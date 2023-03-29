b5266bf6872f8d5df8809db9b2ea07d6ee1a2e34
Shivani Kotikalapudi <shivkoti@unc.edu>
2023-03-29 13:22:08 -0400

    progress

--- exercises/ex07/dictionary_test.py
+++ exercises/ex07/dictionary_test.py
@@ -1,16 +1,21 @@
 __author__: "730556602"
 
+
 """Unit tests for dictionary functions."""
 
-from exercises.ex07.dictionary import invert, favorite_colors, count
+
+from exercises.ex07.dictionary import invert, favorite_color, count
+
 
 def test_empty_dictionary() -> None:
     test_dict: dict[str, str] = {}
     assert invert(test_dict) == {}
 
+
 def test_multiple_colors() -> None:
     test_dict: dict[str, str] = {"Shivani": "green", "Meghan": "green", "Carly": "blue", "Grace": "pink"}
-    assert favorite_colors(test_dict) == "green"
+    assert favorite_color(test_dict) == "green"
+
 
 def test_count() -> None:
     test_list: list[str] = ["a", "a", "b", "c"]
