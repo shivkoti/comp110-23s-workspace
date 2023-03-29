c055a489f6aaf179021829d0c3192e80cb56684e
Shivani Kotikalapudi <shivkoti@unc.edu>
2023-03-29 14:17:10 -0400

    progress

--- exercises/ex07/dictionary.py
+++ exercises/ex07/dictionary.py
@@ -7,26 +7,27 @@ def invert(dict_1: dict[str, str]) -> dict[str, str]:
     """Given dict_1, return new dictionary that inverts the keys and values."""
     new_dict: dict[str, str] = {}
     for key in dict_1:
-        new_dict[dict_1[key]] = key
         if key in new_dict:
-            raise KeyError
+            raise KeyError("This is wrong.")
+        new_dict[dict_1[key]] = key
     return new_dict    
 
     
 def favorite_color(dict_1: dict[str, str]) -> str:
     """Returns the color that appears most in the dictionary."""
     colors: dict[str, int] = {}
-    max_color = []
+    max_color = ""
     max_color_count = 0
     for key in dict_1:
         if dict_1[key] in colors:
             colors[dict_1[key]] += 1
-        elif dict_1[key] not in colors:
+        else:
             colors[dict_1[key]] = 1
-    if colors[dict_1[key]] > max_color_count or colors[dict_1[key]] == max_color_count:
-        max_color.append(dict_1[key])
-        max_color_count == colors[dict_1[key]]
-    return max_color[0]
+    for key in colors:
+        if colors[key] > max_color_count:
+            max_color = [key]
+            max_color_count = colors[key]
+    return max_color
 
 
 def count(list_1: list[str]) -> dict[str, int]:
